5aaf0b839f92ffa7639ad80d570bf29f2a53aa86
Leonard Steppy <leonard.steppy@outlook.de>
2026-07-24 22:18:16 +0200

    Fix data corruption issues and improve read efficiency

--- file.c
+++ file.c
@@ -97,7 +97,9 @@ static int ouichefs_file_get_allocated_blocks(struct inode *inode,
 		const int bno =
 			le32_to_cpu(parent_extent->start) + inner_extent_offset;
 		ret = min_t(unsigned int,
-			    parent_extent->count - inner_extent_offset, nr);
+			    le32_to_cpu(parent_extent->count) -
+				    inner_extent_offset,
+			    nr);
 
 		/* Map the physical block to the given buffer_head */
 		map_bh(bh_result, sb, bno);
@@ -280,32 +282,31 @@ static int ouichefs_write_end(struct file *file, struct address_space *mapping,
 static ssize_t ouichefs_read(struct file *file, char __user *buf, size_t count,
 			     loff_t *pos)
 {
-	int ret;
 	// Nothing to read
 	// Just return 0
 	if (count == 0)
 		return 0;
 
+	struct inode *inode = file->f_inode;
+	struct super_block *sb = inode->i_sb;
+	const size_t blocksize = sb->s_blocksize;
+
 	// If we try to read more than the file has,
 	// just return 0
-	if (*pos >= file->f_inode->i_size) {
+	if (*pos >= inode->i_size) {
 		return 0;
 	}
 
 	// if the request is for more data than the file has,
 	// adjust the count to the available data
-	count = min_t(size_t, count, file->f_inode->i_size - *pos);
+	count = min_t(size_t, count, inode->i_size - *pos);
 
 	// The logical index of the block to read for the current position
-	int iblock = *pos >> file->f_inode->i_sb->s_blocksize_bits;
+	const sector_t iblock = *pos >> sb->s_blocksize_bits;
 
 	// Modulo operation to get the offset within the block
 	// (only works for powers of 2)
-	int offset = *pos & (file->f_inode->i_sb->s_blocksize - 1);
-
-	// Adjust the count to the available data in the block
-	// We can't read more data than the block has
-	count = min_t(size_t, count, file->f_inode->i_sb->s_blocksize - offset);
+	unsigned int offset = *pos & (blocksize - 1);
 
 	// superblock has the field s_blocksize and s_blocksize_bits
 	// s_blocksize is the size of a block in the filesystem (the same as OUICHEFS_BLOCK_SIZE) = 4096
@@ -315,47 +316,64 @@ static ssize_t ouichefs_read(struct file *file, char __user *buf, size_t count,
 
 	struct buffer_head result_bh = {};
 
-	ret = ouichefs_file_get_block(file->f_inode, iblock, &result_bh, 0);
+	ssize_t ret = ouichefs_file_get_allocated_blocks(
+		inode, iblock, (count + offset - 1) / blocksize + 1,
+		&result_bh);
 	if (ret < 0)
 		goto out;
 
 	// If the block is not allocated, return an error
-	if (result_bh.b_blocknr == 0) {
+	if (ret == 0) {
 		ret = -EIO;
 		goto out;
 	}
 
-	struct buffer_head *data_bh =
-		sb_bread(file->f_inode->i_sb, result_bh.b_blocknr);
+	const unsigned int num_allocated_blocks = ret;
+	const size_t bytes_available =
+		num_allocated_blocks * blocksize - offset;
+	count = min_t(size_t, count, bytes_available);
 
-	if (!data_bh) {
-		ret = -EIO;
-		goto out;
+	ssize_t bytes_read = 0;
+	struct buffer_head *data_bh;
+
+	for (int block_offset = 0; block_offset < num_allocated_blocks;
+	     ++block_offset) {
+		data_bh = sb_bread(sb, result_bh.b_blocknr + block_offset);
+
+		if (!data_bh) {
+			ret = bytes_read ? bytes_read : -EIO;
+			goto out;
+		}
+
+		const size_t bytes_remaining = count - bytes_read;
+		const size_t bytes_to_read =
+			min_t(ssize_t, bytes_remaining, blocksize - offset);
+
+		const unsigned long bytes_not_copied =
+			copy_to_user(buf + bytes_read, data_bh->b_data + offset,
+				     bytes_to_read);
+
+		const unsigned long bytes_copied =
+			bytes_to_read - bytes_not_copied;
+
+		// Release the data block buffer head
+		// We don't need it anymore
+		brelse(data_bh);
+
+		// Only case we fail atp is really when the
+		// user space buffer is not valid
+		if (bytes_not_copied == bytes_to_read) {
+			ret = bytes_read ? bytes_read : -EFAULT;
+			goto out;
+		}
+
+		bytes_read += (ssize_t)bytes_copied;
+		offset = 0;
 	}
 
-	int bytes_not_copied =
-		copy_to_user(buf, data_bh->b_data + offset, count);
+	*pos += bytes_read;
 
-	int bytes_copied = count - bytes_not_copied;
-
-	// Release the data block buffer head
-	// We don't need it anymore
-	brelse(data_bh);
-
-	// Only case we fail atp is really when the
-	// user space buffer is not valid
-	if (!bytes_copied) {
-		ret = -EFAULT;
-		goto out;
-	}
-
-	*pos += bytes_copied;
-
-	return bytes_copied;
-
-// out_brelse:
-// 	pr_err("error in out_brelse\n");
-// 	brelse(bh_index);
+	return bytes_read;
 out:
 	pr_err("error in out\n");
 	return ret;
@@ -385,17 +403,17 @@ static ssize_t ouichefs_write(struct file *file, const char __user *buf,
 
 	struct super_block *sb = inode->i_sb;
 	// The logical index of the block to write for the current position
-	sector_t iblock = *pos >> sb->s_blocksize_bits;
+	const sector_t iblock = *pos >> sb->s_blocksize_bits;
 
-	unsigned long s_blocksize = sb->s_blocksize;
+	const unsigned long s_blocksize = sb->s_blocksize;
 	// Modulo operation to get the offset within the block
 	// (only works for powers of 2)
-	int offset = *pos & (s_blocksize - 1);
+	unsigned int offset = *pos & (s_blocksize - 1);
 
 	struct buffer_head result_bh = {};
 
 	// Get the block and allocate it if it's not allocated
-	size_t num_blocks = (count + offset - 1) / s_blocksize + 1;
+	const size_t num_blocks = (count + offset - 1) / s_blocksize + 1;
 	int num_allocated_blocks = ouichefs_file_get_allocated_blocks(
 		inode, iblock, num_blocks, &result_bh);
 	if (num_allocated_blocks == 0) {
@@ -426,8 +444,9 @@ static ssize_t ouichefs_write(struct file *file, const char __user *buf,
 		const size_t bytes_remaining = count - written_bytes;
 		const size_t bytes_to_write =
 			min_t(ssize_t, bytes_remaining, s_blocksize - offset);
-		const size_t bytes_not_copied = copy_from_user(
-			data_bh->b_data + offset, buf, bytes_to_write);
+		const size_t bytes_not_copied =
+			copy_from_user(data_bh->b_data + offset,
+				       buf + written_bytes, bytes_to_write);
 
 		const size_t bytes_copied = bytes_to_write - bytes_not_copied;
 		if (bytes_not_copied == bytes_to_write) {
